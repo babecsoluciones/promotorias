ea3b6bd46f80e29e5e63df51241df72b02a5e6d5
Mario Ernesto Basurto Medrano <mario@d80.mx>
2020-02-26 18:35:50 -0600

    Update buscar-clientes-cotizaciones.php

--- que/buscar-clientes-cotizaciones.php
+++ que/buscar-clientes-cotizaciones.php
@@ -15,27 +15,22 @@ $response = array();
 
 if($_POST['search'] || $_GET['search']){
     $search = $_POST['search'] ? $_POST['search'] : $_GET['search'];
+    
+    $terms = explode(" ",$search);
+    
+    $termino = "";
+    
+    for($i=0;$i<sizeof($terms);$i++)
+    {
+        $termino .= " AND tNombre like '%".$terms[$i]."%' ";
+    }
 
-    $select =   "	SELECT * FROM ( ".
-			" 	SELECT  ".
-			" 	cc.eCodCliente,  ".
-			" 	CONCAT(cc.tNombres, ' ', cc.tApellidos) tCliente,  ".
-			" 	bLibre,  ".
-			" 	su.tNombre as promotor ".
-			" FROM ".
-			" 	CatClientes cc ".
-			" LEFT JOIN SisUsuarios su ON su.eCodUsuario = cc.eCodUsuario".
-            " WHERE 1=1 ".
-			($bAll ? "" : " AND cc.eCodUsuario = ".$_SESSION['sessionAdmin']['eCodUsuario']).
-            " )N1 ".
-            " WHERE 1=1 ".
-            " AND N1.tCliente like '".$search."%'".
-			" ORDER BY N1.eCodCliente ASC";
+    $select =   "	SELECT * FROM CatClientes WHERE 1=1 ".$termino;
     
             $result = mysql_query($select);
     
     while($row = mysql_fetch_array($result)){
-        $response[] = array("value"=>$row['eCodCliente'],"label"=>$row['tCliente'],"bLibre"=>(($row['bLibre']) ? 1 : 2));
+        $response[] = array("value"=>$row['eCodCliente'],"label"=>$row['tCliente']);
     }
 
     echo json_encode($response);
